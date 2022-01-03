3b3571fa06b1acdfabff6a8468b6beca73cfd26e
FerWeisheim <fernandoweisheim@hotmail.com>
2022-01-03 16:07:21 -0300

    fix: url base de datos

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Heroe } from '../interfaces/heroes.interfaces';
 import { Observable } from 'rxjs';
 
-const url = "https://my-json-server.typicode.com/FerWeisheim/heroesApp/heroes"
+const url = "https://my-json-server.typicode.com/FerWeisheim/heroesApp/db.json"
 
 @Injectable({
   providedIn: 'root'
