c6ca0c53dc4e13532b9c510d6c5b4facd6205bcc
FerWeisheim <fernandoweisheim@hotmail.com>
2022-01-05 07:29:41 -0300

    a

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -14,7 +14,7 @@ export class HeroesService {
 
   constructor(private http: HttpClient) { }
   getHeroes(): Observable<Heroe[]>{//obserbabol de arreglo de heroes
-   return this.http.get<Heroe[]>("https://my-json-server.typicode.com/FerWeisheim/heroe/heroe")
+   return this.http.get<Heroe[]>("https://my-json-server.typicode.com/FerWeisheim/heroe/heroes")
   }
 
   getHeroeId(id:string): Observable < Heroe >{
