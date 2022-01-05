827373ed24a9f24443a0481c3258cf72940c91d5
FerWeisheim <fernandoweisheim@hotmail.com>
2022-01-05 03:41:30 -0300

    fix: update url

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -22,16 +22,16 @@ export class HeroesService {
   }
 
   getSugerencias(termino:string): Observable <Heroe[]>{
-    return this.http.get<Heroe[]>(`http://localhost:3000/heroes?q=${termino}&_limit=6`)
+    return this.http.get<Heroe[]>(`url/heroes?q=${termino}&_limit=6`)
   }
   agregarHeroe(heroe:Heroe):Observable<Heroe>{
       return this.http.post<Heroe>(`https://my-json-server.typicode.com/FerWeisheim/heroe/heroes`,heroe)
   }
   actualizandoHeroe(heroe: Heroe):Observable<Heroe>{
-    return this.http.put<Heroe>(`http://localhost:3000/heroes/${heroe.id}`,heroe);
+    return this.http.put<Heroe>(`url/heroes/${heroe.id}`,heroe);
   }
   borrarHeroe(id : string):Observable<any>{
-    return this.http.delete<any>(`http://localhost:3000/heroes/${id}`);
+    return this.http.delete<any>(`url/heroes/${id}`);
   }
   
 }
