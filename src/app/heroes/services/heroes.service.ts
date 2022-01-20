07e7c356588a54fa06bad1675565faf211e99ae1
FerWeisheim <fernandoweisheim@hotmail.com>
2022-01-20 16:53:48 -0300

    fix: asd

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -4,7 +4,7 @@ import { Heroe } from '../interfaces/heroes.interfaces';
 import { Observable } from 'rxjs';
 
   
-const url ="https://my-json-server.typicode.com/FerWeisheim/heroe/heroes";
+// const url ="https://my-json-server.typicode.com/FerWeisheim/heroe/heroes";
 
 @Injectable({
   providedIn: 'root'
@@ -14,24 +14,24 @@ export class HeroesService {
 
   constructor(private http: HttpClient) { }
   getHeroes(): Observable<Heroe[]>{//obserbabol de arreglo de heroes
-   return this.http.get<Heroe[]>("http://my-json-server.typicode.com/FerWeisheim/heroe/heroes")
+   return this.http.get<Heroe[]>("http://localhost:3000/heroes")
   }
 
   getHeroeId(id:string): Observable < Heroe >{
-      return this.http.get<Heroe>(`url/${id}`)
+      return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`)
   }
 
   getSugerencias(termino:string): Observable <Heroe[]>{
-    return this.http.get<Heroe[]>(`https://my-json-server.typicode.com/FerWeisheim/heroe/heroes?q=${termino}&_limit=6`)
+    return this.http.get<Heroe[]>(`http://localhost:3000/heroes?q=${termino}&_limit=6`)
   }
   agregarHeroe(heroe:Heroe):Observable<Heroe>{
-      return this.http.post<Heroe>(`https://my-json-server.typicode.com/FerWeisheim/heroe/heroes`,heroe)
+      return this.http.post<Heroe>(`http://localhost:3000/heroes`,heroe)
   }
   actualizandoHeroe(heroe: Heroe):Observable<Heroe>{
-    return this.http.put<Heroe>(`https://my-json-server.typicode.com/FerWeisheim/heroe/heroes/${heroe.id}`,heroe);
+    return this.http.put<Heroe>(`http://localhost:3000/heroes/${heroe.id}`,heroe);
   }
   borrarHeroe(id : string):Observable<any>{
-    return this.http.delete<any>(`https://my-json-server.typicode.com/FerWeisheim/heroe/heroes/${id}`);
+    return this.http.delete<any>(`http://localhost:3000/heroes/${id}`);
   }
   
 }
